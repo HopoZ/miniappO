c27542d64ca7612f466962b91a27a52ea8fd7d0c
entelechy <phmath41@gmail.com>
2024-04-15 10:33:24 +0800

    update

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -4,27 +4,91 @@ const util = require('../../utils/util.js')
 Page({
   data: {
     logs: [],
-    carData:{},
+    carData: {},
     gradientColor: {
       '0%': '#ffd01e',
       '100%': '#ee0a24',
-    }
+    },
+    carPhotoUrl: ''
   },
-  getDetailedData(){
+  // 获取车辆详细信息
+  getDetailedData() {
     wx.request({
       url: 'http://localhost:8078/web/getDetailedData',
-      method:'GET',
-      data:{
-        carNumber:'MNO345'
+      method: 'GET',
+      data: {
+        carNumber: 'MNO345'
       },
-      success:(res)=>{
-        console.log("res.data: ",res.data)
+      success: (res) => {
+        console.log("res.data: ", res.data);
+        const updatedCarData = res.data;
+        updatedCarData.alcoholConc = (updatedCarData.alcoholConc * 100).toFixed(2);
         this.setData({
-          carData: res.data
-        })
-        // console.log(this.carData) 我犯傻了，异步没考虑就调试
+          carData: updatedCarData
+        }, () => {
+          console.log("carData", this.data.carData);
+          // 在 setData 的回调函数中调用 AddPhoto 方法
+          this.AddPhoto();
+        });
+      }      
+    });
+  },
+  // 获取车辆照片
+  AddPhoto() {
+    const imageKownUrl = 'http://82.156.65.122:8080/photoKnow/';
+    const imageUnknownUrl = 'http://82.156.65.122:8080/photoUnknow/';
+    const carNumber = this.data.carData.carNumber;
+
+    // 尝试获取已知照片
+    wx.downloadFile({
+      url: imageKownUrl + carNumber + '.jpg',
+      success: (res) => {
+        if (res.statusCode === 200) {
+          // 已知照片存在
+          const tempFilePath = res.tempFilePath;
+          // 更新数据
+          this.setData({
+            carPhotoUrl: tempFilePath
+          });
+        } else {
+          // 如果已知照片不存在，则尝试获取未知照片
+          wx.downloadFile({
+            url: imageUnknownUrl + carNumber + '.jpg',
+            success: (res) => {
+              if (res.statusCode === 200) {
+                // 未知照片存在
+                const tempFilePath = res.tempFilePath;
+                // 更新数据
+                this.setData({
+                  carPhotoUrl: tempFilePath
+                });
+              } else {
+                // 未知照片也不存在
+                wx.showToast({
+                  title: '无法获取照片',
+                  icon: 'none',
+                  duration: 2000
+                });
+              }
+            },
+            fail: () => {
+              wx.showToast({
+                title: '无法获取照片',
+                icon: 'none',
+                duration: 2000
+              });
+            }
+          });
+        }
+      },
+      fail: () => {
+        wx.showToast({
+          title: '无法获取照片',
+          icon: 'none',
+          duration: 2000
+        });
       }
-    })
+    });
   },
   onLoad() {
     this.getDetailedData();
